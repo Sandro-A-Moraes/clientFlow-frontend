2dcec261350480cecfe8bdf0aa4699e755bfe02d
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-04-21 18:47:54 -0300

    feat: enhance layout component with styling and add mcp configuration

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import cn from '@/shared/lib/utils/cn';
 
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    <div className={cn(' w-full h-screen flex items-center justify-center')}>
+      layout
+    </div>
+  );
 };
 
 export default layout;
