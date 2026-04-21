b55ad096a3358b3e4e162ac189ef06d6608db4e0
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-04-21 18:58:49 -0300

    feat: refactor layout component and enhance global styles with color variables

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import cn from '@/shared/lib/utils/cn';
 
-const layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={cn(' w-full h-screen flex items-center justify-center')}>
-      layout
+    <div
+      className={cn(
+        'w-full min-h-screen flex items-center justify-center px-6 py-14',
+        'bg-register-mobile',
+      )}
+    >
+      {children}
     </div>
   );
 };
 
-export default layout;
+export default Layout;
