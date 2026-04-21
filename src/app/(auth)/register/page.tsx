3ac13ca389904d936f8aedacf92d9baaafe37176
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-04-21 19:15:31 -0300

    feat: add lucide-react dependency and update register page with logo header

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { CloudCheck } from 'lucide-react';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className='flex flex-col items-center gap-10'>
+      {/* Logo Header */}
+      <div className='flex flex-col items-center gap-2'>
+        <div className='py-1.5 bg-bg-soft w-12 flex items-center justify-center h-8 rounded-xl'>
+          <CloudCheck className=' text-text-link' />
+        </div>
+        <h1>ClientFlow</h1>
+      </div>
+
+      {/* Register Card */}
+      <div></div>
+
+      {/* LinkMark */}
+      <p></p>
+    </div>
+  );
 };
 
 export default page;
