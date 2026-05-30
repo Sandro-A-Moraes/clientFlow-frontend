ccac3f905ff5229e6c3110e53d0dcbd5b48561d0
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-05-30 11:48:36 -0300

    feat: implement ProtectedLayout and Dashboard components for user authentication flow

--- src/app/(auth)/login/hooks/useLogin.ts
+++ src/app/(auth)/login/hooks/useLogin.ts
@@ -1,14 +1,19 @@
 import { authService } from '@/shared/lib/services/auth.service';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 
 export function useLogin() {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: authService.login,
 
-    onSuccess() {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['me'],
+      });
+
       router.push('/dashboard');
     },
   });
